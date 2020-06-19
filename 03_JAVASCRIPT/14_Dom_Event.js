1cabe5212792bc6ac4d82780fcc3c71ecc386408
khairellah <60810477+khairellah@users.noreply.github.com>
2020-06-19 14:51:25 +0100

    Update 14_Dom_Event.js
    Optimizing the code : regrouping the repetition code lines in a function

--- 03_JAVASCRIPT/14_Dom_Event.js
+++ 03_JAVASCRIPT/14_Dom_Event.js
@@ -3,7 +3,8 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul=document.querySelector("ul");
 
-button.addEventListener("click",function(){
+// My Main Function To Add in the UL
+function add(){
     if(input.value.length == 0 )
         {alert("Empty Value would you please write something !");}
     else{    
@@ -17,23 +18,15 @@ button.addEventListener("click",function(){
     // Clear the input
     input.value = "";
 }
-})
+}
 
+//If Click On Button
+button.addEventListener("click", add)
 
+//If Press The Entree Key
 input.addEventListener("keypress",function(event){
     //console.log(event.keyCode);
     if(event.keyCode == 13 ){
-        if(input.value.length == 0 )
-            {alert("Empty Value would you please write something !");}
-        else{    
-        console.log(input.value);
-        //Create the element
-        var li = document.createElement("li");
-        //add the input to li
-        li.appendChild(document.createTextNode(input.value));
-        //attach li to our ul
-        ul.appendChild(li);
-        //Clear the input
-        input.value = "";
-}}
+        add();
+    }
 })
