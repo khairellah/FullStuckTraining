e766e0c079ef7af322af6215c700fc69368d5a47
khairellah <60810477+khairellah@users.noreply.github.com>
2020-07-01 08:10:39 +0100

    create copy
    Copy of the old one with adding just the number to the file'16'

---
+++ 04_ADVANCED_JAVASCRIPT/16_ES7_2016.js
@@ -0,0 +1,21 @@
+// It Hase two (2) Features : .includes & Square
+
+// 1 .includes --> Return Boolean (est-ce qu'il contient ?)
+
+    // For Strings
+'Hello'.includes('ll'); // --> True
+
+    // For Arrays
+    const animals =['cat','dog','bat'];
+
+    animals.includes('cat');    // --> True
+    animals.includes('spider'); // --> False
+
+
+// 2 square or power **
+
+const square = (x) => x**2;;
+const cube = (x) => x**3;
+
+square(2)   // --> 4
+cube(2)     // --> 8  
